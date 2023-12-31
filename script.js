6d9bb365edf29cedc2c17719c3c4f6b04e6bbb13
ana carolina <carolina-macedo93@hotmail.com>
2023-12-30 22:03:01 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Unsplash API
+const count = 10;
+const apiKey = 'zW50gQun7Itc84E1NZgcnT5zmoAdkpJhSyaDNC2KKQk';
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getPhotos();
